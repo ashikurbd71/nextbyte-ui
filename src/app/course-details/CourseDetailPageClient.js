8a52ad34a1b454697bf5a714306ebb0f67793799
ashikurovibd71 <ashikurovi2003@gmail.com>
2025-09-02 23:46:02 +0600

    update

--- src/app/course-details/CourseDetailPageClient.js
+++ src/app/course-details/CourseDetailPageClient.js
@@ -165,7 +165,7 @@ export function CourseDetailPageClient() {
 
                     <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 lg:gap-8">
                         {/* Main Content */}
-                        <div className="lg:col-span-2 order-2 lg:order-1">
+                        <div className="lg:col-span-2 order-1 lg:order-1">
                             {/* Course Preview Video */}
                             <CoursePreview courseData={courseData} />
 
@@ -191,7 +191,7 @@ export function CourseDetailPageClient() {
                         </div>
 
                         {/* Sidebar */}
-                        <div className="lg:col-span-1 order-1 lg:order-2">
+                        <div className="lg:col-span-1 order-2 lg:order-2">
                             <CourseSidebar
                                 courseData={courseData}
                                 paymentLoading={paymentLoading}
